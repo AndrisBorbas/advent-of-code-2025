e1322ab6658c7069183966351365e40d2c7c707e
borbasa <andras.borbas@neocoregames.com>
2025-12-17 16:19:13 +0100

    day3 p1 p2

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,8 +1,61 @@
 import example from "./example.txt";
 import input from "./input.txt";
 
-const data = example.trim().split("\n");
+const data = input.trim().split("\n");
+
+function part1() {
+	let sum = 0;
+	for (const line of data) {
+		let high1 = 0;
+		let high2 = 0;
+		for (let i = 0; i < line.length; i++) {
+			const curr = Number(line[i]);
+			if (curr > high1 && i !== line.length - 1) {
+				if (curr > high2) {
+					high2 = high1;
+				}
+				high1 = curr;
+				high2 = 0;
+			} else if (curr > high2) {
+				high2 = curr;
+			}
+		}
+		// console.log(high1, high2);
+		sum += high1 * 10 + high2;
+	}
+	return sum;
+}
+
+function part2() {
+	let sum = 0;
+	let lineNum = 1;
+	for (const line of data) {
+		let highs = [];
+		for (let i = 0; i < line.length; i++) {
+			const curr = Number(line[i]);
+			highs.push(curr);
+			if (highs.length > 12) {
+				let index = highs.findIndex((v, j) => {
+					if (j === highs.length - 1) return false;
+					return v < highs[j + 1];
+				});
+				if (index === -1) {
+					index = highs.length - 1;
+				}
+				highs.splice(index, 1);
+			}
+		}
+		const joltage = highs.reduce((a, b) => {
+			return a * 10 + b;
+		}, 0);
+		console.log(lineNum, "Joltage: ", joltage, highs, highs.length);
+		sum += joltage;
+		lineNum++;
+	}
+	return sum;
+}
 
 export function day03() {
-	console.log("Day 03:", data);
+	// console.log("Joltage: ", part1());
+	console.log("Joltage: ", part2());
 }
