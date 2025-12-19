0acad15a3d1d55228962da319d74d54a66412b66
borbasa <andras.borbas@neocoregames.com>
2025-12-19 17:16:02 +0100

    day 4 part 2

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -3,7 +3,7 @@ import input from "./input.txt";
 
 const data = input.trim().split("\n");
 
-const map: boolean[][] = data.map((line) =>
+const initialMap: boolean[][] = data.map((line) =>
 	line.split("").map((char) => char === "@"),
 );
 
@@ -19,7 +19,7 @@ function printMap(inputMap: boolean[][]) {
 	}
 }
 
-function countNeighbors(x: number, y: number) {
+function countNeighbors(x: number, y: number, map: boolean[][] = initialMap) {
 	let count = 0;
 	for (let dy = -1; dy <= 1; dy++) {
 		for (let dx = -1; dx <= 1; dx++) {
@@ -37,12 +37,12 @@ function countNeighbors(x: number, y: number) {
 }
 
 function part1() {
-	const access: boolean[][] = new Array(map.length)
+	const access: boolean[][] = new Array(initialMap.length)
 		.fill(false)
-		.map(() => new Array(map[0].length).fill(false));
-	for (let y = 0; y < map.length; y++) {
-		for (let x = 0; x < map[y].length; x++) {
-			if (!map[y][x]) continue;
+		.map(() => new Array(initialMap[0].length).fill(false));
+	for (let y = 0; y < initialMap.length; y++) {
+		for (let x = 0; x < initialMap[y].length; x++) {
+			if (!initialMap[y][x]) continue;
 			access[y][x] = countNeighbors(x, y);
 		}
 	}
@@ -50,12 +50,52 @@ function part1() {
 	const accessCount = access.reduce((sum, row) => {
 		return sum + row.filter((v) => v).length;
 	}, 0);
-	printMap(map);
+	printMap(initialMap);
 	console.log("----");
 	printMap(access);
 	console.log("Access Count: ", accessCount);
 }
 
+function part2() {
+	let accessCount = 0;
+	let total = 0;
+	let debugStep = 0;
+	let access: boolean[][];
+	const currentMap: boolean[][] = initialMap.map((row) => row.slice());
+	printMap(currentMap);
+	do {
+		// reset access map
+		access = new Array(initialMap.length)
+			.fill(false)
+			.map(() => new Array(initialMap[0].length).fill(false));
+		for (let y = 0; y < currentMap.length; y++) {
+			for (let x = 0; x < currentMap[y].length; x++) {
+				if (!currentMap[y][x]) continue;
+				access[y][x] = countNeighbors(x, y, currentMap);
+			}
+		}
+		// Update current map
+		for (let y = 0; y < currentMap.length; y++) {
+			for (let x = 0; x < currentMap[y].length; x++) {
+				if (!currentMap[y][x]) continue;
+				currentMap[y][x] = !access[y][x];
+			}
+		}
+		// console.log("Access Map: ");
+		// printMap(access);
+		// console.log("Current Map: ");
+		// printMap(currentMap);
+		accessCount = access.reduce((sum, row) => {
+			return sum + row.filter((v) => v).length;
+		}, 0);
+		total += accessCount;
+		// console.log("Step Access Count: ", accessCount);
+		// console.log("----");
+	} while (accessCount !== 0 && ++debugStep /* <= 11 */);
+	console.log("Access Count: ", total, debugStep);
+}
+
 export function day04() {
-	part1();
+	// part1();
+	part2();
 }
